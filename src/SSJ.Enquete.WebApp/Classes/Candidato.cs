ff10b1a8514f6b8ec5ba264ec10d6e4bd9f9e671
Bruno N Fernandes <bruno@mercadopleno.com.br>
2022-03-04 00:06:44 -0300

    Commit inicial da enquete

---
+++ src/SSJ.Enquete.WebApp/Classes/Candidato.cs
@@ -0,0 +1,12 @@
+﻿namespace SSJ.Enquete.WebApp.Classes
+{
+    public class Candidato
+    {
+        public string Nome { get; set; }
+        public string ImageUrl { get; set; }
+        public int Votos { get; set; }
+
+        public void Add() => Votos += 1;
+        public void Remove() => Votos -= 1;
+    }
+}
---
+++ src/SSJ.Enquete.WebApp/Classes/Repositorio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SSJ.Enquete.WebApp.Classes
+{
+    public class Repositorio
+    {
+        public readonly List<Candidato> Candidatos = new List<Candidato>();
+
+        public void Adicionar(string nome, string imagemUrl) => Adicionar(new Candidato { Nome = nome, ImageUrl = imagemUrl });
+
+        public void Adicionar(Candidato candidato) => Candidatos.Add(candidato);
+
+        public void Remover(Candidato candidato) => Candidatos.Remove(candidato);
+    }
+}
