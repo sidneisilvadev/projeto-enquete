a64df5d1dff2973097c7cc133f03c50bbef4084b
Bruno N Fernandes <bruno@mercadopleno.com.br>
2022-03-04 09:27:28 -0300

    Novo campo de descrição e adicionando botao + 10

--- src/SSJ.Enquete.WebApp/Classes/Candidato.cs
+++ src/SSJ.Enquete.WebApp/Classes/Candidato.cs
@@ -3,10 +3,12 @@
     public class Candidato
     {
         public string Nome { get; set; }
+        public string Descricao { get; set; }
         public string ImageUrl { get; set; }
         public int Votos { get; set; }
 
         public void Add() => Votos += 1;
+        public void Add10() => Votos += 10;
         public void Remove() => Votos -= 1;
     }
 }
